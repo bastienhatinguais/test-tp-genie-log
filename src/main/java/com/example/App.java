cf0bf6fd863cfd0388b7ae1e207ae334ae54d5c2
Paul Massias <paul.massias@etud.univ-jfc.fr>
2021-11-15 18:22:17 +0100

    Création de conflit de père en fils

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         //Affiche le message "Hello World !"
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello BASTEIN, ya ya ya" );
     }
 }
 //Yoooo
