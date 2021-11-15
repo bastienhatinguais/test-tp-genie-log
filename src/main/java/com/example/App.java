955419fd1d0d3098388c9780eeaecfb6270e9ced
bastienhatinguais <bastien.hatinguais@gmail.com>
2021-11-15 17:21:46 +0100

    Conflit test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         //Affiche le message "Hello World !"
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! Tu es beau" );
     }
 }
 //Yoooo
